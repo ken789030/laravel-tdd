44e7600c018f080842298377611a13c6a0edfe86
ken <ken789030@gmail.com>
2025-04-07 20:24:25 +0800

    init

---
+++ app/Dojo/Tennis.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Dojo;
+
+class Tennis
+{
+
+}
---
+++ tests/Dojo/TennisTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Dojo;
+
+use App\Dojo\Tennis;
+use PHPUnit\Framework\TestCase;
+
+class TennisTest extends TestCase
+{
+
+}
